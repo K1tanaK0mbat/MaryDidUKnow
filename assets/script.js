3fd9426df9f8563ff9943e5da81652bac753d153
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-07 23:01:18 -0600

    questions show on page

--- assets/script.js
+++ assets/script.js
@@ -39,10 +39,13 @@ function startTimer() {
 
 function startQuiz() {
     startTimer();
-    showQuestion();
     startButton.style.display = 'none';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('score').style.display = 'block'; // Show the score element
     quizSection.style.display = 'block';
+    showQuestion();
   }
+  
 
   function showQuestion() {
     var currentQuestion = questions[currentQuestionIndex];
@@ -59,20 +62,59 @@ function startQuiz() {
     }
   }
   
-  function checkAnswer(selectedAnswer) {
-    var currentQuestion = questions[currentQuestionIndex];
-    if (selectedAnswer === currentQuestion.answers[currentQuestion.correctIndex]) {
-    } else {
+ // ... (previous code)
 
-      timerCount -= 8; 
+function checkAnswer(selectedAnswer) {
+    var currentQuestion = questions[currentQuestionIndex];
+    var correctAnswer = currentQuestion.answers[currentQuestion.correctIndex];
+    var answerElements = document.querySelectorAll('#answers li');
+  
+    for (var i = 0; i < answerElements.length; i++) {
+      // Check if the answer is correct
+      if (currentQuestion.answers[i] === correctAnswer) {
+        answerElements[i].classList.add('correct-answer');
+      }
+  
+      // Check if the answer is selected
+      if (currentQuestion.answers[i] === selectedAnswer) {
+        if (selectedAnswer === correctAnswer) {
+          // Mark the selected answer as correct
+          answerElements[i].classList.add('selected-correct');
+          showFeedbackMessage('Correct!');
+        } else {
+          // Mark the selected answer as incorrect
+          answerElements[i].classList.add('selected-incorrect');
+          showFeedbackMessage('Wrong!');
+          timerCount -= 10; // Subtract 10 seconds for incorrect answer
+        }
+      }
     }
   
     currentQuestionIndex++;
   
     if (currentQuestionIndex < questions.length) {
-      showQuestion();
+      setTimeout(function () {
+        resetAnswerStyles(answerElements);
+        showQuestion();
+      }, 1000); 
     } else {
       clearInterval(timer);
 
     }
   }
+  
+  function resetAnswerStyles(answerElements) {
+    answerElements.forEach(function (answerElement) {
+      answerElement.classList.remove('correct-answer', 'selected-correct', 'selected-incorrect');
+    });
+  }
+  
+  function showFeedbackMessage(message) {
+    var feedbackElement = document.createElement('p');
+    feedbackElement.textContent = message;
+    document.getElementById('quiz').appendChild(feedbackElement);
+    setTimeout(function () {
+      feedbackElement.remove();
+    }, 1000);
+  }
+  
