a3d53097c84bcf4fb4302e295a457e63804dfb91
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-07 22:32:00 -0600

    added index code

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,18 @@
-var timer = document.querySelectorAll('.time');
-var startButtton = document.querySelector('.startButton');
+var timerEl = document.querySelectorAll('.time');
+var startButton = document.querySelector('.startButton');
+
+startButton.addEventListener("click", startQuiz);
+var timer;
+var timerCount;
+var questions = ["Question1", "Question2", "Question3", "Question4", "Question5"];
+
+function startTimer() {
+    timerCount = 75;
+    timer = setInterval(function () {
+        timerCount--;
+        timerEl.textContent = timerCount;
+        if (timerCount === 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
